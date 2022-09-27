5a25f8be453b19d3c21ad2ab3a3125cb697e22f0
DreamMaHaiBin <991225723@q.com>
2022-09-27 08:49:27 +0800

    新增

--- src/components/dateClearDateSettlement/index.jsx
+++ src/components/dateClearDateSettlement/index.jsx
@@ -1,13 +1,23 @@
 import React, { Component } from "react";
 import './common.scss'
+import { Input } from 'antd';
 // 烧结日清日结
-import { tableListData } from '../util/listLable'
+import { tableListDataShaojie } from '../util/shaojieTable'
 export default class DateClearDateSettlement extends Component {
     constructor(props) {
         super(props)
         this.state = {
-
+            data: tableListDataShaojie
         }
+        // this.changeTabaleData = this.changeTabaleData.bind(this)
+    }
+
+    changeTabaleData(name, index, value) {
+        console.log(name, index, value);
+        const newData = this.state.data
+        newData[index][name] = value
+        this.setState({data:newData})
+       
     }
     render() {
         return (
@@ -42,32 +52,33 @@ export default class DateClearDateSettlement extends Component {
                             <td>月成本</td>
                         </tr>
                         {
-                            tableListData.map((obj, index) => {
+                            this.state.data.map((obj, index) => {
                                 return (
                                     <tr key={index} className={
                                         index === 0 ? 'date-clear-date-settlemen-table-blue' :
-                                        index === 1 || index === 2 || index === 3 || index === 7 || index === 11 || index === 12 || index === 16 ? 'date-clear-date-settlemen-table-yellow' :
-                                        'date-clear-date-settlemen-table-none'
+                                            index === 1 || index === 2 || index === 3 || index === 7 || index === 11 || index === 12 || index === 16 ? 'date-clear-date-settlemen-table-yellow' :
+                                                'date-clear-date-settlemen-table-none'
                                     }>
-                                        <td>{obj.name}</td>
-                                        <td>{obj.dw}</td>
-                                        <td>{obj.dj}</td>
-                                        <td>{obj.qcrxh}</td>
-                                        <td>{obj.qcyxh}</td>
-                                        <td>{obj.qcrcb}</td>
-                                        <td>{obj.qcycb}</td>
-                                        <td>{obj.ysrxh}</td>
-                                        <td>{obj.ysyxh}</td>
-                                        <td>{obj.ysrcb}</td>
-                                        <td>{obj.ysycb}</td>
-                                        <td>{obj.esrxh}</td>
-                                        <td>{obj.esyxh}</td>
-                                        <td>{obj.esrcb}</td>
-                                        <td>{obj.esycb}</td>
-                                        <td>{obj.ssrxh}</td>
-                                        <td>{obj.ssyxh}</td>
-                                        <td>{obj.ssrcb}</td>
-                                        <td>{obj.ssycb}</td>
+                                        <td><Input value={obj.name} onChange={() => this.changeTabaleData("name", index, obj.name)} /></td>
+                                        <td><Input value={obj.dw} onChange={() => this.changeTabaleData("dw", index, obj.dw)} /></td>
+                                        <td><Input value={obj.dj} onChange={() => this.changeTabaleData("dj", index, obj.dj)} /></td>
+                                        <td><Input value={obj.qcrxh} onChange={() => this.changeTabaleData("qcrxh", index, obj.qcrxh)} /></td>
+                                        <td><Input value={obj.qcyxh} onChange={() => this.changeTabaleData("qcyxh", index, obj.qcyxh)} /></td>
+                                        <td><Input value={obj.qcrcb} onChange={() => this.changeTabaleData("qcrcb", index, obj.qcrcb)} /></td>
+                                        <td><Input value={obj.qcycb} onChange={() => this.changeTabaleData("qcycb", index, obj.qcycb)} /></td>
+                                        <td><Input value={obj.ysrxh} onChange={() => this.changeTabaleData("ysrxh", index, obj.ysrxh)} /></td>
+                                        <td><Input value={obj.ysyxh} onChange={() => this.changeTabaleData("ysyxh", index, obj.ysyxh)} /></td>
+                                        <td><Input value={obj.ysrcb} onChange={() => this.changeTabaleData("ysrcb", index, obj.ysrcb)} /></td>
+                                        <td><Input value={obj.ysycb} onChange={() => this.changeTabaleData("ysycb", index, obj.ysycb)} /></td>
+                                        <td><Input value={obj.esrxh} onChange={() => this.changeTabaleData("esrxh", index, obj.esrxh)} /></td>
+                                        <td><Input value={obj.esyxh} onChange={() => this.changeTabaleData("esyxh", index, obj.esyxh)} /></td>
+                                        <td><Input value={obj.esrcb} onChange={() => this.changeTabaleData("esrcb", index, obj.esrcb)} /></td>
+                                        <td><Input value={obj.esycb} onChange={() => this.changeTabaleData("esycb", index, obj.esycb)} /></td>
+                                        <td><Input value={obj.ssrxh} onChange={() => this.changeTabaleData("ssrxh", index, obj.ssrxh)} /></td>
+                                        <td><Input value={obj.ssyxh} onChange={() => this.changeTabaleData("ssyxh", index, obj.ssyxh)} /></td>
+                                        <td><Input value={obj.ssrcb} onChange={() => this.changeTabaleData("ssrcb", index, obj.ssrcb)} /></td>
+                                        <td><Input value={obj.ssycb} onChange={() => this.changeTabaleData("ssycb", index, obj.ssycb)} /></td>
+
                                     </tr>
                                 )
                             })
